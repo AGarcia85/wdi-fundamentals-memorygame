3dfaf9feed08e966a2ddd47bb9c233fb3323de6d
Alex Garcia <afgarcia666@gmail.com>
2019-11-04 16:11:27 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,8 @@
 var cards = [
 {
-	rank: "queen",
-	suit: "hearts",
-	cardImage: "images/queen-of-hearts.png"
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage: 'images/queen-of-hearts.png'
 },
 {
 	rank: "queen",
@@ -10,14 +10,14 @@ var cards = [
 	cardImage: "images/queen-of-diamonds.png"
 },
 {
-	rank: "king",
-	suit: "hearts",
-	cardImage: "images/king-of-hearts.png"
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: 'images/king-of-hearts.png'
 },
 {
-	rank: "king",
-	suit: "diamonds",
-	cardImage: "images/king-of-diamonds.png"
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage: 'images/king-of-diamonds.png'
 }
 ];
 var cardsInPlay = [];
@@ -30,15 +30,30 @@ var checkForMatch = function() {
 	}
 }
 
-var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
+var flipCard = function() {
+	cardId = this.getAttribute('data-id');
+	console.log(cardId);
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
+		cardsInPlay = [];
 	}
 }
 
-flipCard(0)
-flipCard(2)
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+
+	}
+
+function reset() {
+	document.getElementByTagName('button').addEventListener('click', reset);
+}
+}
+
+createBoard();
